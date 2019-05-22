a1eede1b735fb5cb694aa15ace57f6a4e7ec714d
aeisha simmons <ashicks2017@outlook.com>
2019-05-22 05:18:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
  low_value = nil
  name_hash.each do |key, value|
    if low_value == nil || v < low_value
-     low_value = v 
-     low_key = k
+     low_value = value
+     low_key = key
     end
   end
   low_key
