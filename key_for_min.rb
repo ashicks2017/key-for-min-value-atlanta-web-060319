2492fad4b0a4d269af4afc4ec3322815ec4d2087
aeisha simmons <ashicks2017@outlook.com>
2019-05-22 05:17:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
  name_hash = {:candy => 1, :chips => 5, :soda => 2}
+ 
 def key_for_min_value(name_hash)
  low_key = nil 
  low_value = nil
